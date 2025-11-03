27f976d3d43d972261688407c0bd5c552dfde9a6
LucasPelanda <lucas.pelanda06@gmail.com>
2025-11-03 21:56:53 +0000

    Adicionei aplicaçao

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+APP = Flask(__name__)
+
+
+@APP.get("/")
+
+
+def index():
+    return "<h1>Olá mundo </h1>"
+
+if __name__ == '__main__':
+    APP.run(host='0.0.0.0', port=80)
+
+    
